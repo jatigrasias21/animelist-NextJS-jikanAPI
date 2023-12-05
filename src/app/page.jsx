49a6c05c2df33ad9bb9847263fa24f895b8d3b6b
jatigrasias@gmail.com <jatigrasias@gmail.com>
2023-12-05 09:55:02 +0700

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,10 @@
 import AnimeList from "@/components/AnimeList"
 import Header from "@/components/AnimeList/Header"
-import Hero from "@/components/AnimeList/Hero"
 import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/libs/api-libs"
 
 const Page = async () => {
-
-  //const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
-  //const topAnime = await response.json()
-
   const topAnime = await getAnimeResponse("top/anime", "limit=10")
-  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime","entry")
+  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
   recommendedAnime = reproduce(recommendedAnime, 5)
 
   return (
